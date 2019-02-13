89d01506e4fc3deb5f212b1a62370d507009aba6
Jaidev <jaidevtrumpet@gmail.com>
2019-02-13 18:03:18 +0530

    Finished Interests

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 $(document).on("scroll", function() {
 
-  if($(document).scrollTop()>150) {
+  if($(document).scrollTop()>75) {
     $("nav").addClass("bg-blue shrink");
+    $("a h1").addClass("txt-header");
+    $("nav div ul li a").addClass("txt-header");
   } else {
     $("nav").removeClass("bg-blue shrink");
+    $("a h1").removeClass("txt-header");
+    $("nav div ul li a").removeClass("txt-header");
   }
 });
