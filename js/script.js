2ce0dbd185fe073ce0369727f7906ac7ad3b2676
Jaidev <jaidevtrumpet@gmail.com>
2019-03-17 23:33:30 +0530

    Adding Scroll Reveal

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(document).on("scroll", function() {
 
-  if($(document).scrollTop()>75) {
+  if($(document).scrollTop()>25) {
     $("nav").addClass("bg-blue shrink");
     $("a h1").addClass("txt-header");
     $("nav div ul li a").addClass("txt-header");
@@ -10,3 +10,9 @@ $(document).on("scroll", function() {
     $("nav div ul li a").removeClass("txt-header");
   }
 });
+
+
+ScrollReveal().reveal('.reveal0', {easing: 'ease-in-out'});
+ScrollReveal().reveal('.reveal5', {delay:500, easing: 'ease-in-out'});
+ScrollReveal().reveal('.reveal10', {delay:1000, easing: 'ease-in-out'});
+ScrollReveal().reveal('.reveal15', {delay:1500, easing: 'ease-in-out'});
